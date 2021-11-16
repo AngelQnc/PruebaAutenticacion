5d71779b3d5588d5bd03b20ff4e0bbc310edbc93
AngelQnc <88949903+AngelQnc@users.noreply.github.com>
2021-11-16 13:54:27 -0600

    Add files via upload

---
+++ init.js
@@ -0,0 +1,9 @@
+firebase.initializeApp({
+  apiKey: "AIzaSyDm9wnABDRSZdiCpfCJ0Q2ATiikpubX1jU",
+  authDomain: "autenticacion-c4b98.firebaseapp.com",
+  projectId: "autenticacion-c4b98",
+  storageBucket: "autenticacion-c4b98.appspot.com",
+  messagingSenderId: "1026161460666",
+  appId: "1:1026161460666:web:b1085b1724c7c5522e0983",
+  measurementId: "G-KMF1XVVGX0"
+});
